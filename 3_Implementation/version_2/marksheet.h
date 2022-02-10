2d047bef550ea5364f9be5180b874cff738299fd
Indrajith-07 <indrajithkesti6692@gmail.com>
2022-02-10 17:18:20 +0530

    addes,#1

---
+++ 3_Implementation/version_2/marksheet.h
@@ -0,0 +1,15 @@
+#ifndef MARKSHEET_H_
+#define MARKSHEET_H_
+
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdio.h>
+
+void input();
+int calculations();
+void show();
+void result();
+void remark();
+
+#endif
---
+++ 3_Implementation/version_2/marksheet_main.c
@@ -0,0 +1,11 @@
+#include"marksheet.h"
+
+int main()
+{
+    input();
+    calculations();
+    result();
+    remark();
+    show();
+    return 0;
+}

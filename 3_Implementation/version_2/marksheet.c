c3792355ee862419edecbeec35589704972f2e01
Indrajith-07 <indrajithkesti6692@gmail.com>
2022-02-10 19:37:49 +0530

    added,#1

--- 3_Implementation/version_2/marksheet.c
+++ 3_Implementation/version_2/marksheet.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
-#include "marksheet.h"
 
+void input();
+int calculations();
+void show();
+void result();
+void remark();
 int r,sem,subjects,total,marks[20],temp;
 float per;
-char b[20],ch[20],a[50],d[30],c[20],grade,res[5],rem[16],sub[20][20];
+char ay[100],b[100],ch[100],a[100],d[100],c[100],grade,res[5],rem[16],sub[20][20];
 char s[] = "pass";
 char s1[] = "fail";
 char s3[] = "Congratulations";
 char s4[] = " ";
+
+
 void input()
 {
     printf("Enter your name : ");
@@ -16,22 +22,28 @@ void input()
     gets(b);
     printf("Enter clg name : ");
     gets(a);
+    printf("Enter AcademicYear (yyyy-yy):");
+    gets(ay);
     printf("Enter DATE of Birth(dd/mm/yyyy) : ");
     gets(d);
     printf("Enter ROLL NO : ");
     scanf("%d",&r);
     printf("Enter Semester : ");
     scanf("%d",&sem);
+    if(sem>10)
+    {
+        printf("Program Terminated\nPlease enter correct number of semester");
+        exit(1);
+    }
     printf("Enter Course : ");
     scanf("%s",&c);
     printf("Enter Number of Subjects: ");
     scanf("%d",&subjects);
     if(subjects>10)
     {
-        printf("Program Terminated\n");
+        printf("Program Terminated\nPlease enter correct number of subjects");
         exit(1);
     }
-
     for(int i=0;i<subjects;i++)
     {
         printf("\n Enter the subject ");
@@ -48,33 +60,36 @@ void input()
 
 void show()
 {
-    int i=0,i1=0,j=0,j1=0,k=0,k1=0,l=0,l1=0;
+    int i=0,i1=0,j=0,j1=0,j2=0,k=0,k1=0,l=0,l1=0;
     while(i1<113)
     {
         printf("*");
         i1++;
     }
     printf("\n|");
-    printf("\n| \t\t\t %s ",c);
+    printf("\n| \t\t\t COLLEGE NAME : %s",a);
     printf("\n|\n");
-    printf("\n| \t\t\t %s ",a);
+    printf("\n| \t\t\t COURSE :%s",c);
+    printf("\n|\n");
+    printf("\n| \t\t\t Academic Year :%s",ay);
     printf("\n|\n");
     while(i<113)
     {
         printf("-");
         i++;
     }
-    printf("\n| NAME : %s \t \t",ch);
-    printf("\n| DATE OF BIRTH : %s \t\t\t\t\t\t PLACE : %s",d,b);
-    printf("\n| CLG NAME : %s \t \t ",a);
-    printf("\n| ROLL NO. : %d \t",r);
-    printf("\n| Semester : %d \n",sem);
+    printf("\n| NAME : %s",ch);
+    printf("\n| DATE OF BIRTH : %s",d);
+    printf("\n| CLG NAME : %s",a);
+    printf("\n| PLACE : %s",b);
+    printf("\n| ROLL NO. : %d",r);
+    printf("\n| Semester : %d\n",sem);
     while(j<113)
     {
         printf("-");
         j++;
     }
-    printf("\n|\tSUBJECTS\t\t\t\t|obtained marks\t\t\t|Total\t\t\t\t|\n");
+    printf("\n|\tSUBJECTS\t\t\t\t|\tobtained marks\t\t|\tTotal\t\t\t|\n");
     while(k<113)
     {
         printf("-");
@@ -82,20 +97,20 @@ void show()
     }
     for(int i=0;i<subjects;i++)
     {
-    printf("\n|\t %s \t\t\t\t\t| %d\t\t\t\t| 100\t\t\t\t|\n",sub+1+i,marks[i]);
+    printf("\n|\t %s \t\t\t\t\t| \t%d\t\t\t|\t100\t\t\t|\n",sub+1+i,marks[i]);
     }
     while(l<113)
     {
         printf("-");
         l++;
     }
-    printf("\n|\tTotal marks obtained\t\t| \t Out of Marks\t | PERCENTAGE\t| \t Result | GRADE |\n");
+    printf("\n|\tTotal marks obtained\t\t| \t Out of Marks\t | PERCENTAGE\t| \t Result |   GRADE   |\n");
     while(l1<113)
     {
         printf("-");
         l1++;
     }
-    printf("\n|\t\t%d\t\t\t| \t %d\t\t | %.2f\t| \t %s\t| %c |\n",total,(subjects*100),per,res,grade);
+    printf("\n|\t\t%d\t\t\t| \t %d\t\t | %.2f\t| \t %s \t|     %c   |\n",total,(subjects*100),per,res,grade);
     while(k1<113)
     {
         printf("*");
@@ -110,12 +125,16 @@ void show()
         j1++;
     }
     printf("\n\n\n");
+    printf("\n\t\tPrincipal      \t\t\t    Student\n");
+     while(j2<113)
+    {
+        printf("*");
+        j2++;
+    }
+    printf("\n\n\n");
     printf("\n\t\t\t\t\t\tMADE BY INDRAJITH :) \n");
-//printf("\n\n\n");
-/*while(z<113){
-  printf("*");
-  z++;}*/
 }
+
 int calculations()
 {
 

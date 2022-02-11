686bc2cda2edce709d6660c1094c09de74c2420e
Indrajith-07 <98821876+Indrajith-07@users.noreply.github.com>
2022-02-11 10:07:25 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    printf("Welcome to Github Actions.\n********* Its Working*******\n");
+    printf("****************Welcome to Github Actions.\n********* Its Working*******\n");
     return 0;
 }
